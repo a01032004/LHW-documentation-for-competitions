77c9a8afc411e47067c21b5d9b1715f9faa2471a
pavlov <sin5900@mail.ru>
2024-05-30 10:38:10 +0300

    Произведен небольшой рефакторинг кода

--- Interactive-window/window.py
+++ Interactive-window/window.py
@@ -16,17 +16,18 @@ mainframe.pack(side="top", anchor="w")
 
 
 # Создаем функцию для создания таблицы
-def create_table(title, table : Team):
+def create_table(team : Team):
     frame = tkinter.Frame(mainframe)
     frame.pack(side=tkinter.LEFT)
 
-    label0 = tkinter.Label(frame, text=title)
+    # Генерация шапки
+    label0 = tkinter.Label(frame, text=team.team_name)
     label1 = tkinter.Label(frame, text='id')
     label2 = tkinter.Label(frame, text="name")
     label3 = tkinter.Label(frame, text="age")
     label4 = tkinter.Label(frame, text="sex")
 
-    label0.grid(row=0, column=0, columnspan=4,stick="we")
+    label0.grid(row=0, column=0, columnspan=4, stick="we")
     label1.grid(row=1, column=0)
     label2.grid(row=1, column=1)
     label3.grid(row=1, column=2)
@@ -37,11 +38,12 @@ def create_table(title, table : Team):
     frame.grid_columnconfigure(2, minsize=50)
     frame.grid_columnconfigure(3, minsize=50)
 
-    for i in range(0, table.arr.__len__()):
-        tkinter.Label(frame, text=table.arr[i].id).grid(row=i + 2, column=0)
-        tkinter.Label(frame, text=table.arr[i].name).grid(row=i + 2, column=1)
-        tkinter.Label(frame, text=table.arr[i].age).grid(row=i + 2, column=2)
-        tkinter.Label(frame, text=table.arr[i].sex).grid(row=i + 2, column=3)
+    # Вывод участников команды
+    for i in range(0, team.arr.__len__()):
+        tkinter.Label(frame, text=team.arr[i].id).grid(row=i + 2, column=0)
+        tkinter.Label(frame, text=team.arr[i].name).grid(row=i + 2, column=1)
+        tkinter.Label(frame, text=team.arr[i].age).grid(row=i + 2, column=2)
+        tkinter.Label(frame, text=team.arr[i].sex).grid(row=i + 2, column=3)
 
 
 def create_window():
@@ -51,15 +53,15 @@ def create_window():
 part1 = Participant("M", 20, "Dima", 1)
 part2 = Participant("Z", 21, "Yana", 2)
 
-tabla = Team([part1, part2])
+team = Team([part1, part2])
 
 
 # Создаем фреймы для каждой таблицы
-create_table("Команда 1", tabla)
+create_table(team)
 
-create_table("Команда 2", tabla)
+create_table(team)
 
-create_table("Команда 3", tabla)
+create_table(team)
 
 # Запускаем главный цикл обработки событий
 root.mainloop()
