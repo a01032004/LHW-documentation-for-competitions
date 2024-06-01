68432e7aa8bc9d61bc039117a5e236f8ff226e08
a01032004 <116118135+a01032004@users.noreply.github.com>
2024-06-01 21:50:30 +0300

    Доделал шапку стартового протокола

--- createTeams.py
+++ createTeams.py
@@ -17,42 +17,69 @@ sheet = wb['Первый лист']
 
 # создаем именованный стиль
 ns = NamedStyle(name='highlight')
-ns.font = Font(bold=True, size=20)
+ns.font = Font(bold=True, size=18)
 border = Side(style='thick', color='000000')
 ns.border = Border(left=border, top=border, right=border, bottom=border)
-
+wb.add_named_style(ns)
 #sheet['A1'].style = 'highlight'
 #начинаем формирование шапки стартового фацла
 
-sheet.merge_cells('A1:H1')
+sheet.merge_cells('A1:I1')
 sheet['A1'].value = 'СТАРТОВЫЙ ПРОТОКОЛ'
 sheet['A1'].alignment = Alignment(horizontal='center')
 
-sheet.merge_cells('A2:H2')
+sheet.merge_cells('A2:I2')
 sheet['A2'].value = 'СОРЕВНОВАНИЙ ПО ЛЫЖНЫМ ГОНКАМ'
 sheet['A2'].alignment = Alignment(horizontal='center')
 
-sheet.merge_cells('A3:H3')
+sheet.merge_cells('A3:I3')
 sheet['A3'].value = 'СПАРТАКИАДЫ А'
 sheet['A3'].alignment = Alignment(horizontal='center')
 
-sheet.merge_cells('A4:H4')
+sheet.merge_cells('A4:I4')
 #получаем группу соревнований
 type_competition = 1
 if (type_competition == 1):
-    sheet['A4'].value = 'cреди постоянного состава'
+    sheet['A4'].value = 'среди постоянного состава'
 if (type_competition == 2):
-    sheet['A4'].value = 'cреди переменного состава (мужчины)'
+    sheet['A4'].value = 'среди переменного состава (мужчины)'
 if (type_competition == 3):
-    sheet['A4'].value = 'cреди переменного состава (женщины)'
+    sheet['A4'].value = 'среди переменного состава (женщины)'
 sheet['A4'].alignment = Alignment(horizontal='center')
+
+
+sheet.merge_cells('A6:D6')
+sheet['A6'].value = 'Дата проведения - 22.01.2024'
+sheet['A6'].alignment = Alignment(horizontal='left')
+
+sheet.merge_cells('E6:I6')
+sheet['E6'].value = 'Одинцовский парк культуры спорта и отдыха'
+sheet['E6'].alignment = Alignment(horizontal='left')
+
+sheet.merge_cells('A7:C7')
+sheet['A7'].value = 'Время старта - 14:50'
+sheet['A7'].alignment = Alignment(horizontal='left')
+
+sheet.merge_cells('A8:D8')
+if (type_competition == 1):
+    sheet['A8'].value = 'Дистанция - 5 км (м), 3 км (ж)'
+if (type_competition == 2):
+    sheet['A8'].value = 'Дистанция - 5 км'
+if (type_competition == 3):
+    sheet['A8'].value = 'Дистанция - 3 км'
+sheet['A8'].alignment = Alignment(horizontal='left')
+
+sheet.merge_cells('E8:F8')
+sheet['E8'].value = 'Стиль - свободный'
+sheet['E8'].alignment = Alignment(horizontal='left')
+
 
 
 
 # записываем файл
 wb.save('start.xlsx')
 
-wb.add_named_style(ns)
+
 
 
 
