6ee8e0018e983d25f5dd4100a8b0994e5af33634
a01032004 <116118135+a01032004@users.noreply.github.com>
2024-06-01 21:23:59 +0300

    Начал заполнять шапку

--- createTeams.py
+++ createTeams.py
@@ -2,6 +2,7 @@ from classes.Participant import Participant
 from classes.Team import Team
 import openpyxl
 from openpyxl.styles import NamedStyle, Font, Border, Side
+from openpyxl.styles import Alignment
 
 '''book = openpyxl.open("first.xlsx", read_only=True)
 sheet = book.active'''
@@ -21,6 +22,30 @@ border = Side(style='thick', color='000000')
 ns.border = Border(left=border, top=border, right=border, bottom=border)
 
 #sheet['A1'].style = 'highlight'
+#начинаем формирование шапки стартового фацла
+
+sheet.merge_cells('A1:H1')
+sheet['A1'].value = 'СТАРТОВЫЙ ПРОТОКОЛ'
+sheet['A1'].alignment = Alignment(horizontal='center')
+
+sheet.merge_cells('A2:H2')
+sheet['A2'].value = 'СОРЕВНОВАНИЙ ПО ЛЫЖНЫМ ГОНКАМ'
+sheet['A2'].alignment = Alignment(horizontal='center')
+
+sheet.merge_cells('A3:H3')
+sheet['A3'].value = 'СПАРТАКИАДЫ А'
+sheet['A3'].alignment = Alignment(horizontal='center')
+
+sheet.merge_cells('A4:H4')
+#получаем группу соревнований
+type_competition = 1
+if (type_competition == 1):
+    sheet['A4'].value = 'cреди постоянного состава'
+if (type_competition == 2):
+    sheet['A4'].value = 'cреди переменного состава (мужчины)'
+if (type_competition == 3):
+    sheet['A4'].value = 'cреди переменного состава (женщины)'
+sheet['A4'].alignment = Alignment(horizontal='center')
 
 
 
