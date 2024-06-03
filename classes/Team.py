ffcc03a66afc3c3058294b164db1bf6f270dad26
pavlov <sin5900@mail.ru>
2024-06-03 14:22:47 +0300

    Названия переведены на русский. Пофикшен баг с изменением времени

--- classes/Team.py
+++ classes/Team.py
@@ -2,10 +2,10 @@ from classes.Participant import Participant
 
 
 class Team:
-    def __init__(self, participant_arr=[], team_name="Команда без названия", team_something=""):
+    def __init__(self, participant_arr=[], team_name="Команда без названия", team_number=""):
         self.arr = participant_arr
         self.team_name = team_name
-        self.team_something = team_something
+        self.team_number = team_number
 
     def add_participant(self, participant):
         self.arr.append(participant)
